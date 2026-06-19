068d1c50811c4dd88a94765ed96eeb3c710bb8e4
Jorge Ramirez <radijo.alitq@gmail.com>
2026-06-19 16:00:59 -0600

    Reset SITE_DESCRIPTION to an empty string
    Clear the SITE_DESCRIPTION constant.

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jorge Ramirez';
-export const SITE_DESCRIPTION = 'Postdoctoral Research Fellow at UAQ.';
+export const SITE_DESCRIPTION = '';
 
 export const CONTACT = {
   organization: 'Universidad Autónoma de Querétaro',
